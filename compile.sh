cd0b299307a8aa9e78243d48ad1d9c10df48b1a3
Marek Miller <marek.l.miller@gmail.com>
2015-08-20 20:34:20 +0200

    Enable multiple indeces. Many minor corrections.

--- compile.sh
+++ compile.sh
@@ -4,18 +4,26 @@ MAIN="MMiller_PhDthesis"
 
 MAIN_TEXFILE="${MAIN}.tex"
 
-
 LATEX="pdflatex"
 LATEX_CMD="-halt-on-error -synctex=1"
 BIBTEX="bibtex"
 BIBTEX_CMD=""
 
+MAKEINDEX="makeindex"
+MAKEINDEX_CMD=""
+
 date +"%Y%m%d%H%M%S" > ./compile.timestamp
 
+echo "### Compiling LaTeX sources..."
 ${LATEX} ${LATEX_CMD} ${MAIN_TEXFILE}
 
+echo "### BibTeX"
 ${BIBTEX} ${BIBTEX_CMD} ${MAIN} && \
     ${LATEX} ${LATEX_CMD} ${MAIN_TEXFILE}
+
+echo "### make index andd index of symbols"
+${MAKEINDEX} ${MAKEINDEX_CMD} idx
+${MAKEINDEX} ${MAKEINDEX_CMD} sym
 
 exit 0
 
